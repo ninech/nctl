e44656a4fa5b136339e16789692322c282a0dd16
thde <thde@users.noreply.github.com>
2024-03-15 10:55:49 +0100

    tests(redis): use table tests

--- update/redis_test.go
+++ update/redis_test.go
@@ -8,45 +8,58 @@ import (
 	storage "github.com/ninech/apis/storage/v1alpha1"
 	"github.com/ninech/nctl/api"
 	"github.com/ninech/nctl/internal/test"
+	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
-func TestRedis(t *testing.T) {
-	max := storage.RedisMaxMemoryPolicy("noeviction")
-	cmd := redisCmd{
-		Name: "test",
-		MaxMemoryPolicy: &max,
-	}
 
-	redis :=  test.Redis("test", "default", "nine-es34")
+func Test_redisCmd_Run(t *testing.T) {
+	tests := []struct {
+		name    string
+		cmd     redisCmd
+		wantErr bool
+	}{
+		{"simple", redisCmd{}, false},
+		{"memorySize", redisCmd{MemorySize: &storage.RedisMemorySize{Quantity: resource.MustParse("1G")}}, false},
+		{"maxMemoryPolicy", redisCmd{MaxMemoryPolicy: ptr.To(storage.RedisMaxMemoryPolicy("noeviction"))}, false},
+		{"allowedCIDRs", redisCmd{AllowedCIDRs: &[]storage.IPv4CIDR{storage.IPv4CIDR("0.0.0.0/0")}}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.cmd.Name = "test-" + t.Name()
 
-	scheme, err := api.NewScheme()
-	if err != nil {
-		t.Fatal(err)
-	}
-	apiClient := &api.Client{WithWatch: fake.NewClientBuilder().WithScheme(scheme).Build(), Project: "default"}
-	ctx := context.Background()
+			scheme, err := api.NewScheme()
+			if err != nil {
+				t.Fatal(err)
+			}
+			apiClient := &api.Client{WithWatch: fake.NewClientBuilder().WithScheme(scheme).Build(), Project: "default"}
+			ctx := context.Background()
 
-	if err := apiClient.Create(ctx, redis); err != nil {
-		t.Fatalf("redis create error, got: %s", err)
-	}
-	if err := apiClient.Get(ctx, api.ObjectName(redis), redis); err != nil {
-		t.Fatalf("expected redis to exist, got: %s", err)
-	}
+			redis :=  test.Redis(tt.cmd.Name, apiClient.Project, "nine-es34")
+			if err := apiClient.Create(ctx, redis); err != nil {
+				t.Fatalf("redis create error, got: %s", err)
+			}
+			if err := apiClient.Get(ctx, api.ObjectName(redis), redis); err != nil {
+				t.Fatalf("expected redis to exist, got: %s", err)
+			}
 
-	if err := cmd.Run(ctx, apiClient); err != nil {
-		t.Fatal(err)
-	}
-	if err := apiClient.Get(ctx, api.ObjectName(redis), redis); err != nil {
-		t.Fatalf("expected redis to exist, got: %s", err)
-	}
-	if !reflect.DeepEqual(redis.Spec.ForProvider.MemorySize, cmd.MemorySize) {
-		t.Fatalf("expected redis.Spec.ForProvider.MemorySize = %v, got: %v", cmd.MemorySize, redis.Spec.ForProvider.MemorySize)
-	}
-	if cmd.MaxMemoryPolicy != nil && !reflect.DeepEqual(redis.Spec.ForProvider.MaxMemoryPolicy, *cmd.MaxMemoryPolicy) {
-		t.Fatalf("expected redis.Spec.ForProvider.MaxMemoryPolicy = %v, got: %v", cmd.MemorySize, redis.Spec.ForProvider.MemorySize)
-	}
-	if cmd.AllowedCIDRs != nil &&!reflect.DeepEqual(redis.Spec.ForProvider.AllowedCIDRs, *cmd.AllowedCIDRs) {
-		t.Fatalf("expected redis.Spec.ForProvider.AllowedCIDRs = %v, got: %v", cmd.MemorySize, redis.Spec.ForProvider.MemorySize)
+			if err := tt.cmd.Run(ctx, apiClient); (err != nil) != tt.wantErr {
+				t.Errorf("redisCmd.Run() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if err := apiClient.Get(ctx, api.ObjectName(redis), redis); (err != nil) != tt.wantErr {
+				t.Fatalf("expected redis to exist, got: %s", err)
+			}
+			if !reflect.DeepEqual(redis.Spec.ForProvider.MemorySize, tt.cmd.MemorySize) {
+				t.Fatalf("expected redis.Spec.ForProvider.MemorySize = %v, got: %v", tt.cmd.MemorySize, redis.Spec.ForProvider.MemorySize)
+			}
+			if tt.cmd.MaxMemoryPolicy != nil && !reflect.DeepEqual(redis.Spec.ForProvider.MaxMemoryPolicy, *tt.cmd.MaxMemoryPolicy) {
+				t.Fatalf("expected redis.Spec.ForProvider.MaxMemoryPolicy = %v, got: %v", *tt.cmd.MaxMemoryPolicy, redis.Spec.ForProvider.MaxMemoryPolicy)
+			}
+			if tt.cmd.AllowedCIDRs != nil && !reflect.DeepEqual(redis.Spec.ForProvider.AllowedCIDRs, *tt.cmd.AllowedCIDRs) {
+				t.Fatalf("expected redis.Spec.ForProvider.AllowedCIDRs = %v, got: %v", *tt.cmd.AllowedCIDRs, redis.Spec.ForProvider.AllowedCIDRs)
+			}
+		})
 	}
 }
