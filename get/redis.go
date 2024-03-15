505b729b7a53880b3f725e9fd72f49b1f9a09d41
Josua Bryner <16536859+josi19@users.noreply.github.com>
2024-03-15 11:48:42 +0100

    print redis connection secret for get command

--- get/redis.go
+++ get/redis.go
@@ -2,6 +2,7 @@ package get
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"text/tabwriter"
 
@@ -10,18 +11,27 @@ import (
 	"github.com/ninech/nctl/internal/format"
 )
 
-type redisCmd struct{}
+type redisCmd struct {
+	Name       string `arg:"" help:"Name of the Redis Instance to get. If omitted all in the project will be listed." default:""`
+	PrintToken bool   `help:"Print the bearer token of the Account. Requires name to be set." default:"false"`
+}
 
 func (cmd *redisCmd) Run(ctx context.Context, client *api.Client, get *Cmd) error {
 	redisList := &storage.RedisList{}
 
-	if err := get.list(ctx, client, redisList); err != nil {
+	if err := get.list(ctx, client, redisList, matchName(cmd.Name)); err != nil {
 		return err
 	}
 
 	if len(redisList.Items) == 0 {
 		printEmptyMessage(os.Stdout, storage.RedisKind, client.Project)
 		return nil
+	}
+
+	if len(cmd.Name) != 0 {
+		if cmd.PrintToken {
+			return cmd.printToken(ctx, client, &redisList.Items[0])
+		}
 	}
 
 	switch get.Output {
@@ -48,4 +58,14 @@ func printRedisInstances(list []storage.Redis, get *Cmd, header bool) error {
 	}
 
 	return w.Flush()
+}
+
+func (cmd *redisCmd) printToken(ctx context.Context, client *api.Client, redis *storage.Redis) error {
+	token, err := getConnectionSecret(ctx, client, "token", redis)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%s\n", token)
+	return nil
 }
