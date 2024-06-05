4c84e4bce7f0466c6db57ff3a48754e3fba862d6
Cyrill Troxler <cyrill@nine.ch>
2024-06-05 13:08:14 +0200

    feat: add auth print-access-token command
    This is a convenience function to print the API client access token to
    stdout. Mainly useful when testing things on the API with curl and maybe
    some scriping use-cases.

--- auth/auth.go
+++ auth/auth.go
@@ -1,11 +1,12 @@
 package auth
 
 type Cmd struct {
-	Login      LoginCmd      `cmd:"" help:"Login to nineapis.ch."`
-	Logout     LogoutCmd     `cmd:"" help:"Logout from nineapis.ch."`
-	Cluster    ClusterCmd    `cmd:"" help:"Authenticate with Kubernetes Cluster."`
-	OIDC       OIDCCmd       `cmd:"" help:"Perform interactive OIDC login." hidden:""`
-	SetProject SetProjectCmd `cmd:"" help:"Set the default project to be used."`
-	SetOrg     SetOrgCmd     `cmd:"" help:"Set the organization to be used."`
-	Whoami     WhoAmICmd     `cmd:"" help:"Show who you are logged in as, your active organization and all your available organizations."`
+	Login            LoginCmd            `cmd:"" help:"Login to nineapis.ch."`
+	Logout           LogoutCmd           `cmd:"" help:"Logout from nineapis.ch."`
+	Cluster          ClusterCmd          `cmd:"" help:"Authenticate with Kubernetes Cluster."`
+	OIDC             OIDCCmd             `cmd:"" help:"Perform interactive OIDC login." hidden:""`
+	SetProject       SetProjectCmd       `cmd:"" help:"Set the default project to be used."`
+	SetOrg           SetOrgCmd           `cmd:"" help:"Set the organization to be used."`
+	Whoami           WhoAmICmd           `cmd:"" help:"Show who you are logged in as, your active organization and all your available organizations."`
+	PrintAccessToken PrintAccessTokenCmd `cmd:"" help:"Print short-lived access token to authenticate against the API to stdout and exit."`
 }
