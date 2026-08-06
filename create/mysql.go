720cda521122b9bd811775a02abe055eac26c4ad
Marco Streich <marco@nine.ch>
2026-08-06 09:11:00 +0200

    feat(mysql): add --mysql-version flag to create command
    as a two-year lifecycle has now been established

--- create/mysql.go
+++ create/mysql.go
@@ -23,6 +23,7 @@ type mySQLCmd struct {
 	MachineType           string            `placeholder:"${mysql_machine_default}" help:"Sizing for a particular MySQL instance. Available types: ${mysql_machine_types}"`
 	AllowedCidrs          []meta.IPv4CIDR   `placeholder:"203.0.113.1/32" help:"IP addresses allowed to connect to the instance."`
 	DatabaseSSHKeysFlags  `set:"ssh_keys_purpose=allowed to connect to the database server in order to up-/download and directly restore database backups"`
+	MysqlVersion          storage.MySQLVersion                   `placeholder:"${mysql_version_default}" help:"Release version with which the MySQL instance is created. Available versions: ${mysql_versions}"`
 	SQLMode               *[]storage.MySQLMode                   `placeholder:"\"MODE1, MODE2, ...\"" help:"Configures the sql_mode setting. Modes affect the SQL syntax MySQL supports and the data validation checks it performs. Defaults to: ${mysql_mode}"`
 	CharacterSetName      string                                 `placeholder:"${mysql_charset}" help:"Configures the character_set_server variable."`
 	CharacterSetCollation string                                 `placeholder:"${mysql_collation}" help:"Configures the collation_server variable."`
@@ -87,6 +88,7 @@ func (cmd *mySQLCmd) newMySQL(namespace string) (*storage.MySQL, error) {
 				MachineType:  infra.NewMachineType(cmd.MachineType),
 				AllowedCIDRs: []meta.IPv4CIDR{}, // avoid missing parameter error
 				SSHKeys:      sshKeys,
+				Version:      cmd.MysqlVersion,
 				SQLMode:      cmd.SQLMode,
 				CharacterSet: storage.MySQLCharacterSet{
 					Name:      cmd.CharacterSetName,
@@ -116,6 +118,8 @@ func MySQLKongVars() kong.Vars {
 	result["mysql_location_options"] = strings.Join(stringSlice(storage.MySQLLocationOptions), ", ")
 	result["mysql_location_default"] = string(storage.MySQLLocationDefault)
 	result["mysql_user"] = string(storage.MySQLUser)
+	result["mysql_version_default"] = string(storage.MySQLVersionDefault)
+	result["mysql_versions"] = strings.Join(stringSlice(storage.MySQLVersions), ", ")
 	result["mysql_mode"] = strings.Join(storage.MySQLModeDefault, ", ")
 	result["mysql_long_query_time"] = string(storage.MySQLLongQueryTimeDefault)
 	result["mysql_charset"] = string(storage.MySQLCharsetDefault)
